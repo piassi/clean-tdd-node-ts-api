1c409eee582f8ad198c3a162bcd481fffacdffdd
Thales Nunes <thales.piassi@ifood.com.br>
2021-02-16 16:56:09 -0300

    chore: jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  roots: ['<rootDir>/tests'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
