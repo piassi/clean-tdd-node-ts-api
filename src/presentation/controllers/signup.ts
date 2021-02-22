d31681829ab8757683a5cedba25b5d0e95425240
Thales Nunes <thales.piassi@ifood.com.br>
2021-02-22 20:04:59 -0300

    test: ensure SignUpController return Error in body if no email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,19 @@
 export class SignUpController {
   handle (httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name')
+    const { body } = httpRequest
+
+    if (!body.name) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: name')
+      }
+    }
+
+    if (!body.email) {
+      return {
+        statusCode: 400,
+        body: new Error('Missing param: email')
+      }
     }
   }
 }
