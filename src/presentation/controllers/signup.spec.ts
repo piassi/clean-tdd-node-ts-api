2af4d854a6d476befcfc7ba3cf09288433f1d537
Thales Nunes <thales.piassi@ifood.com.br>
2021-02-22 19:56:27 -0300

    test: ensure SignUpController returns 400 if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,17 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  it('should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any@mail.com',
+        password: '654654',
+        passwordConfirmation: '654654'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
