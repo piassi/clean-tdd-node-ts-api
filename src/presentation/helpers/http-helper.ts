6b84dc4c44019983ff9e22dfc1c885e8058bd71b
Thales Nunes <thales.piassi@ifood.com.br>
2021-02-28 21:51:19 -0300

    refactor: create serverError helper

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -1,8 +1,16 @@
+import { ServerError } from '../errors/server-error'
 import { HttpResponse } from '../protocols/http'
 
 export function badRequest (error: Error): HttpResponse {
   return {
     statusCode: 400,
     body: error
+  }
+}
+
+export function serverError (message: string): HttpResponse {
+  return {
+    statusCode: 500,
+    body: new ServerError(message)
   }
 }
