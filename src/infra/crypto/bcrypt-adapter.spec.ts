d16321cd55ce6b97665a78123e39e01e62ad7de0
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-03 17:06:48 -0300

    test: ensure BcryptAdapter returns a string on success

--- src/infra/crypto/bcrypt-adapter.spec.ts
+++ src/infra/crypto/bcrypt-adapter.spec.ts
@@ -10,4 +10,13 @@ describe('Bcrypt adapter', () => {
     await sut.encrypt(value)
     expect(bcryptHashSpy).toHaveBeenCalledWith(value, salt)
   })
+
+  it('should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    jest.spyOn(bcrypt, 'hash').mockResolvedValueOnce('hashed_value')
+    const value = 'any_value'
+    const hashed = await sut.encrypt(value)
+    expect(hashed).toEqual('hashed_value')
+  })
 })
