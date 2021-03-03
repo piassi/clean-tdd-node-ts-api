47dbcc4666c86b016b55977f6dfb73228a830269
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-02 21:39:32 -0300

    test: ensure DbAddAccount calls AddAccountRepository with correct values

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -1,12 +1,21 @@
 import { AccountModel } from '../../../domain/models/account'
 import { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account'
+import { AddAccountRepository } from '../../protocols/add-account-repository'
 import { Encrypter } from '../../protocols/encrypter'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly encrypter: Encrypter) {}
+  constructor (
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository) {}
 
   async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
-    return Promise.resolve(null)
+    const password = await this.encrypter.encrypt(account.password)
+
+    const newAccountData = {
+      ...account,
+      password
+    }
+
+    return this.addAccountRepository.add(newAccountData)
   }
 }
