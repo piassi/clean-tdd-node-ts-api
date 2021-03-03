fc8ff5e844af7f4fc740402306f1b83999ee77fa
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-03 17:09:31 -0300

    refactor: move sut to factory

--- src/infra/crypto/bcrypt-adapter.spec.ts
+++ src/infra/crypto/bcrypt-adapter.spec.ts
@@ -1,10 +1,25 @@
 import bcrypt from 'bcrypt'
+import { Encrypter } from '../../data/protocols/encrypter'
 import { BcryptAdapter } from './bcrypt-adapter'
+
+interface SutTypes {
+  sut: Encrypter
+  salt: number
+}
+
+const makeSut = (): SutTypes => {
+  const salt = 12
+  const sut = new BcryptAdapter(salt)
+
+  return {
+    sut,
+    salt
+  }
+}
 
 describe('Bcrypt adapter', () => {
   it('should call bcrypt lib with correct arguments', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const { sut, salt } = makeSut()
     const bcryptHashSpy = jest.spyOn(bcrypt, 'hash')
     const value = 'any_value'
     await sut.encrypt(value)
@@ -12,8 +27,7 @@ describe('Bcrypt adapter', () => {
   })
 
   it('should return a hash on success', async () => {
-    const salt = 12
-    const sut = new BcryptAdapter(salt)
+    const { sut } = makeSut()
     jest.spyOn(bcrypt, 'hash').mockResolvedValueOnce('hashed_value')
     const value = 'any_value'
     const hashed = await sut.encrypt(value)
