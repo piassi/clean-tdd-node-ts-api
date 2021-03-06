3d5bec996f0c999b04a8eba2aa004c37d76416b0
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-06 14:20:23 -0300

    feat: add SignUpController factory

---
+++ src/main/factories/signup.ts
@@ -0,0 +1,14 @@
+import { SignUpController } from '../../presentation/controllers/signup'
+import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
+import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../infra/crypto/bcrypt-adapter'
+import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+
+export const MakeSignUpController = (): SignUpController => {
+  const salt = 12
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const emailValidatorAdapter = new EmailValidatorAdapter()
+  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+}
