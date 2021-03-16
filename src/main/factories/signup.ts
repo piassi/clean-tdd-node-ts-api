d6fc6db6a3d723884488dc56daf39f736e7c253c
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-16 09:17:11 -0300

    test: ensure LogMongoRepository insert error on db

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -3,12 +3,17 @@ import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/crypto/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from '../decorators/LogControllerDecorator'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
+import { Controller } from '../../presentation/protocols'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const accountMongoRepository = new AccountMongoRepository()
   const bcryptAdapter = new BcryptAdapter(salt)
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
   const emailValidatorAdapter = new EmailValidatorAdapter()
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const logErrorRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logErrorRepository)
 }
