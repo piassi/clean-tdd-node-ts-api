ad0c0c9285b2a52ff36fb710c4c7bb6e9dcaf9e9
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-16 09:11:51 -0300

    feat: ensure LogMongoRepository insert error on db

---
+++ src/infra/db/mongodb/log-repository/log.ts
@@ -0,0 +1,12 @@
+import { LogErrorRepository } from '../../../../data/protocols/log-error-repository'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class LogMongoRepository implements LogErrorRepository {
+  async logError (stack: string): Promise<void> {
+    const logsCollection = await MongoHelper.getCollection('logs')
+    await logsCollection.insertOne({
+      stack,
+      date: new Date()
+    })
+  }
+}
