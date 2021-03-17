7c6d3508aff6414f9611c69f833e51b50614ca69
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-17 20:39:29 -0300

    feat: ensure LoginController returns badRequest if an invalid email is provided

--- src/presentation/controllers/login.ts
+++ src/presentation/controllers/login.ts
@@ -1,13 +1,19 @@
-import { MissingParamError } from '../errors'
+import { InvalidParamError, MissingParamError } from '../errors'
 import { badRequest } from '../helpers/http-helper'
-import { Controller, HttpRequest, HttpResponse } from '../protocols'
+import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols'
 
 export class LoginController implements Controller {
+  constructor (private readonly emailValidator: EmailValidator) {}
+
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const { body } = httpRequest
 
     if (!body.email) {
       return badRequest(new MissingParamError('email'))
+    }
+
+    if (!this.emailValidator.isValid(body.email)) {
+      return badRequest(new InvalidParamError('email'))
     }
 
     if (!body.password) {
