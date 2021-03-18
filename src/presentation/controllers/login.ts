f3cb28f2312c49caae8df90e4d3999a883bcc745
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-18 08:41:40 -0300

    feat: ensure LoginController returns serverError in case of throw

--- src/presentation/controllers/login.ts
+++ src/presentation/controllers/login.ts
@@ -1,25 +1,29 @@
 import { InvalidParamError, MissingParamError } from '../errors'
-import { badRequest } from '../helpers/http-helper'
+import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller, EmailValidator, HttpRequest, HttpResponse } from '../protocols'
 
 export class LoginController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { body } = httpRequest
+    try {
+      const { body } = httpRequest
 
-    if (!body.email) {
-      return badRequest(new MissingParamError('email'))
+      if (!body.email) {
+        return badRequest(new MissingParamError('email'))
+      }
+
+      if (!this.emailValidator.isValid(body.email)) {
+        return badRequest(new InvalidParamError('email'))
+      }
+
+      if (!body.password) {
+        return badRequest(new MissingParamError('password'))
+      }
+
+      return Promise.resolve(null)
+    } catch (error) {
+      return serverError(error)
     }
-
-    if (!this.emailValidator.isValid(body.email)) {
-      return badRequest(new InvalidParamError('email'))
-    }
-
-    if (!body.password) {
-      return badRequest(new MissingParamError('password'))
-    }
-
-    return Promise.resolve(null)
   }
 }
