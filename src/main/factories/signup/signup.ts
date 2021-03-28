9a54905ba71057db09b3de93de5156e8e3f0734d
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-28 11:30:53 -0300

    refactor: move signup factory to its own directory

---
+++ src/main/factories/signup/signup.ts
@@ -0,0 +1,18 @@
+import { SignUpController } from '../../../presentation/controllers/signup'
+import { DbAddAccount } from '../../../data/usecases/add-account/db-add-account'
+import { BcryptAdapter } from '../../../infra/crypto/bcrypt-adapter'
+import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
+import { LogControllerDecorator } from '../../decorators/LogControllerDecorator'
+import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
+import { Controller } from '../../../presentation/protocols'
+import { makeSignUpValidation } from './signup-validation'
+
+export const makeSignUpController = (): Controller => {
+  const salt = 12
+  const accountMongoRepository = new AccountMongoRepository()
+  const bcryptAdapter = new BcryptAdapter(salt)
+  const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
+  const signUpController = new SignUpController(dbAddAccount, makeSignUpValidation())
+  const logErrorRepository = new LogMongoRepository()
+  return new LogControllerDecorator(signUpController, logErrorRepository)
+}
