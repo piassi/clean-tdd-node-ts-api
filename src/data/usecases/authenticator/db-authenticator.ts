4cae985b9ab46c84369bb1ee8d2ee70ea32f9423
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-30 09:10:42 -0300

    feat: ensure DbAuthenticator call HashComparer with correct values

--- src/data/usecases/authenticator/db-authenticator.ts
+++ src/data/usecases/authenticator/db-authenticator.ts
@@ -1,9 +1,11 @@
 import { AuthCredentials, Authenticator } from '../../../domain/usecases/authenticator'
+import { HashComparer } from '../../protocols/crypto/hash-comparer'
 import { LoadAccountByEmailRepository } from '../../protocols/db/load-account-by-email-repository'
 
 export class DbAuthenticator implements Authenticator {
   constructor (
-    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository
+    private readonly loadAccountByEmailRepository: LoadAccountByEmailRepository,
+    private readonly hashComparer: HashComparer
   ) {}
 
   async auth (credentials: AuthCredentials): Promise<string> {
@@ -12,6 +14,8 @@ export class DbAuthenticator implements Authenticator {
     if (!user) {
       return null
     }
+
+    await this.hashComparer.compare(credentials.password, user.password)
 
     return null
   }
