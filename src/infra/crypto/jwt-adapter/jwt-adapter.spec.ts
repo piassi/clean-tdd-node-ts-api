dba5a6c78f836c71829a3ae9167d45db961d8f14
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-31 11:12:59 -0300

    test: ensure JwtAdapter call jwt sign with correct value and secret

--- src/infra/crypto/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/crypto/jwt-adapter/jwt-adapter.spec.ts
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
 describe('JwtAdapter', () => {
-  it('should call jwt method with correct values', async () => {
+  it('should call jwt sign with correct value and secret', async () => {
     const valueMock = 'any_value'
     const secretMock = 'any_secret'
 
