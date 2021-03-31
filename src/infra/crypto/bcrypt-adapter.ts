80752894b9ac847ff759c3367f62b44a68cd9fd0
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-31 10:31:35 -0300

    feat: ensure BcryptAdapter.compare return bcrypt compare result

--- src/infra/crypto/bcrypt-adapter.ts
+++ src/infra/crypto/bcrypt-adapter.ts
@@ -10,7 +10,6 @@ export class BcryptAdapter implements Hasher, HashComparer {
   }
 
   async compare (value: string, hash: string): Promise<boolean> {
-    await bcrypt.compare(value, hash)
-    return Promise.resolve(true)
+    return await bcrypt.compare(value, hash)
   }
 }
