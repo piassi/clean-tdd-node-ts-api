6f0edbe742c8411f3f759e6ae04aab059b7b9b26
Thales Nunes <thales.piassi@ifood.com.br>
2021-03-31 11:20:03 -0300

    test: ensure JwtAdapter.encrypt return jwt.sign result

--- src/infra/crypto/jwt-adapter/jwt-adapter.spec.ts
+++ src/infra/crypto/jwt-adapter/jwt-adapter.spec.ts
@@ -2,16 +2,43 @@ import jwt from 'jsonwebtoken'
 import { JwtAdapter } from './jwt-adapter'
 
 describe('JwtAdapter', () => {
-  it('should call jwt sign with correct value and secret', async () => {
+  type SutTypes = {
+    sut: JwtAdapter
+    valueMock: string
+    secretMock: string
+  }
+
+  const makeSut = (): SutTypes => {
     const valueMock = 'any_value'
     const secretMock = 'any_secret'
 
     const sut = new JwtAdapter(secretMock)
 
-    const signSpy = jest.spyOn(jwt, 'sign')
+    return {
+      sut,
+      valueMock,
+      secretMock
+    }
+  }
 
-    await sut.encrypt(valueMock)
+  describe('encrypt', () => {
+    it('should call jwt sign with correct value and secret', async () => {
+      const { sut, valueMock, secretMock } = makeSut()
+      const signSpy = jest.spyOn(jwt, 'sign')
 
-    expect(signSpy).toHaveBeenCalledWith(valueMock, secretMock)
+      await sut.encrypt(valueMock)
+
+      expect(signSpy).toHaveBeenCalledWith(valueMock, secretMock)
+    })
+
+    it('should return jwt.sign result', async () => {
+      const { sut, valueMock } = makeSut()
+      const signResultMock = 'mocked_result'
+      jest.spyOn(jwt, 'sign').mockImplementationOnce(() => signResultMock)
+
+      const encryptResult = await sut.encrypt(valueMock)
+
+      expect(encryptResult).toEqual(signResultMock)
+    })
   })
 })
