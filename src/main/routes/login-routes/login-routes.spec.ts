5ad4fe8eef6fa21836c4c1d00696f4049416df07
Thales Nunes <thales.piassi@ifood.com.br>
2021-04-01 21:42:46 -0300

    test: ensure /login return 200 status with JWT on success

---
+++ src/main/routes/login-routes/login-routes.spec.ts
@@ -0,0 +1,44 @@
+import { Collection } from 'mongodb'
+import request from 'supertest'
+import { BcryptAdapter } from '../../../infra/crypto/bcrypt-adapter/bcrypt-adapter'
+import { MongoHelper } from '../../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../../config/app'
+
+let accountsCollection: Collection
+
+beforeAll(async () => {
+  await MongoHelper.connect(process.env.MONGO_URL)
+})
+
+afterAll(async () => {
+  await MongoHelper.disconnect()
+})
+
+beforeEach(async () => {
+  accountsCollection = await MongoHelper.getCollection('accounts')
+  await accountsCollection.deleteMany({})
+})
+
+describe('Login Routes', () => {
+  describe('/login', () => {
+    it('should return 200 status with JWT on success', async () => {
+      const hasher = new BcryptAdapter(12)
+      const password = await hasher.hash('756adasd')
+
+      await accountsCollection.insertOne({
+        name: 'Test',
+        email: 'email@email.com',
+        password
+      })
+
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'email@email.com',
+          password: '756adasd'
+        })
+        .expect(200)
+        .expect('Content-Length', '99')
+    })
+  })
+})
