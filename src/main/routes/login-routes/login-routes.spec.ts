f0cc22a0095a53389e4bb160dc3b388ce398b54b
Thales Nunes <thales.piassi@ifood.com.br>
2021-04-01 21:45:03 -0300

    feat: ensure /login return 401 status if user does not exists

--- src/main/routes/login-routes/login-routes.spec.ts
+++ src/main/routes/login-routes/login-routes.spec.ts
@@ -59,5 +59,15 @@ describe('Login Routes', () => {
         })
         .expect(401)
     })
+
+    it('should return 401 status if user does not exists', async () => {
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'email@email.com',
+          password: '123'
+        })
+        .expect(401)
+    })
   })
 })
